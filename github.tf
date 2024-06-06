4d03efcb7f13465e764f9914d34315dafad9e227
Max Schaefer <max@excloo.com>
2024-06-06 22:36:16 +1000

    change secrets

--- github.tf
+++ github.tf
@@ -2,61 +2,6 @@ data "github_user" "default" {
   username = ""
 }
 
-resource "github_actions_secret" "portainer_cloudflare_api_tokens" {
-  for_each = {
-    for k, website in local.websites : k => website
-    if website.app_type == "portainer"
-  }
-
-  plaintext_value = jsonencode(local.cloudflare_api_tokens)
-  repository      = each.value.name
-  secret_name     = "CLOUDFLARE_API_TOKENS"
-}
-
-resource "github_actions_secret" "portainer_database_passwords" {
-  for_each = {
-    for k, website in local.websites : k => website
-    if website.app_type == "portainer"
-  }
-
-  plaintext_value = jsonencode(local.database_passwords)
-  repository      = each.value.name
-  secret_name     = "DATABASE_PASSWORDS"
-}
-
-resource "github_actions_secret" "portainer_resend_api_keys" {
-  for_each = {
-    for k, website in local.websites : k => website
-    if website.app_type == "portainer"
-  }
-
-  plaintext_value = jsonencode(local.resend_api_keys_merged)
-  repository      = each.value.name
-  secret_name     = "RESEND_API_KEYS"
-}
-
-resource "github_actions_secret" "portainer_secret_hashes" {
-  for_each = {
-    for k, website in local.websites : k => website
-    if website.app_type == "portainer"
-  }
-
-  plaintext_value = jsonencode(local.secret_hashes)
-  repository      = each.value.name
-  secret_name     = "SECRET_HASHES"
-}
-
-resource "github_actions_secret" "portainer_websites" {
-  for_each = {
-    for k, website in local.websites : k => website
-    if website.app_type == "portainer"
-  }
-
-  plaintext_value = jsonencode(local.websites)
-  repository      = each.value.name
-  secret_name     = "WEBSITES"
-}
-
 resource "github_actions_variable" "portainer_portainer_url" {
   for_each = {
     for k, website in local.websites : k => website
@@ -68,15 +13,15 @@ resource "github_actions_variable" "portainer_portainer_url" {
   value         = each.value.url
 }
 
-resource "github_actions_variable" "portainer_servers" {
+resource "github_actions_secret" "portainer_stacks" {
   for_each = {
     for k, website in local.websites : k => website
     if website.app_type == "portainer"
   }
 
-  repository    = each.value.name
-  variable_name = "SERVERS"
-  value         = jsonencode(local.servers_merged)
+  plaintext_value = jsonencode(local.websites)
+  repository      = each.value.name
+  secret_name     = "STACKS"
 }
 
 resource "github_repository_file" "gatus_config" {
