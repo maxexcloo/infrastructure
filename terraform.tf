8ff9c2cf94fc4fa8e84ea7e68aed345e5061aef9
Max Schaefer <max@excloo.com>
2024-08-06 14:28:46 +1000

    move state

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,13 @@
 terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "Excloo"
+
+    workspaces {
+      name = "OpenTofu"
+    }
+  }
+
   required_providers {
     b2 = {
       source = "backblaze/b2"
